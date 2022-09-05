6621cfb0c5856a82fd813b842fe541d53d6edf97
allenINO03 <111472758+allenINO03@users.noreply.github.com>
2022-09-05 10:18:40 +0800

    Create app.java

---
+++ exam_1/app.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) {
+ 
+        // Object Me
+
+    student Me = new student();
+    
+    Me.surname = "Inocencio";
+    Me.firstname = "Allen";
+    Me.middleinital = "Y";
+    Me.studentNumber = 2022100256;
+    Me.studentEmailAddress = "inocencioay@students.national-u.edu.ph";
+    Me.iAmAwsome = true;
+    Me.sayMyEmailAddress();
+    Me.sayMyStudentNumber();
+    Me.iAmAwsome();
+
+
+
+
+        
+    }
+
+
+
+}
